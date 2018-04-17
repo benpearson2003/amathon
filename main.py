a9c33f827b85462fb885a43410f4db0ece45fb7c
Benjamin Pearson <bpearson@arklegaudit.gov>
2018-04-17 10:17:01 -0500

    finds reusable, transparent designs on Google resizes for Amazon Merch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 from PIL import Image, ImageDraw
+import scrapeImages
+
+scrapeImages.run('doodle')
 width = 4500
 height = 5400
 inputDirectory = 'art'
@@ -13,12 +16,12 @@ for filename in os.listdir(inputDirectory):
     artW, artH = art.size
     halfWidth = width/2
     if(artW < halfWidth):
-        art = art.resize((halfWidth,int(float(halfWidth)/artW*artH)))
+        art = art.resize((int(halfWidth),int(halfWidth/artW*artH)))
     artW, artH = art.size
 
     # create blank shirt and paste art
     shirt = Image.new('RGBA',(width, height))
-    shirt.paste(art, ((width-artW)/2,(height-artH)/2))
+    shirt.paste(art, (int((width-artW)/2),int((height-artH)/10)))
 
     # save shirt
     shirt.save(outputDirectory + '/' + os.path.splitext(filename)[0] + '_shirt.png', 'png')
